b0eee9cf507be227176f1f59dcbe69efa143b8d4
tonyliao <tonynowater87@gmail.com>
2021-08-29 22:26:05 +0800

    Added tile shortcut

---
+++ app/src/main/java/com/tonynowater/qr_scanner_to_sms/QRSmsScannerTileService.kt
@@ -0,0 +1,36 @@
+package com.tonynowater.qr_scanner_to_sms
+
+import android.os.Build
+import android.service.quicksettings.Tile
+import android.service.quicksettings.TileService
+import androidx.annotation.RequiresApi
+
+@RequiresApi(api = Build.VERSION_CODES.N)
+class QRSmsScannerTileService : TileService() {
+    override fun onClick() {
+        super.onClick()
+        val launchIntent = packageManager.getLaunchIntentForPackage(packageName)
+        if (launchIntent != null) {
+            startActivity(launchIntent)
+        }
+    }
+
+    override fun onTileAdded() {
+        super.onTileAdded()
+        val tile = qsTile
+        tile.state = Tile.STATE_INACTIVE
+        tile.updateTile()
+    }
+
+    override fun onTileRemoved() {
+        super.onTileRemoved()
+    }
+
+    override fun onStartListening() {
+        super.onStartListening()
+    }
+
+    override fun onStopListening() {
+        super.onStopListening()
+    }
+}
